dbd323f51ff9f11d849176d7fb26f1ece2a9bca1
xiaohang <xiaohang317@126.com>
2020-12-14 16:08:08 +0800

    小案例大分析

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,12 @@
 // 手机号 加星
-export function processingTel(tel:string){
+export function processingTel(tel: string) {
     tel = "" + tel;
     var ary = tel.split("");
-    ary.splice(3,4,"****");
-    var tel1=ary.join("");
+    ary.splice(3, 4, "****");
+    var tel1 = ary.join("");
     return tel1
 }
+
 // 时间 格式化成 2018-12-12 12:12:00
 export function timestampToTime(timestamp: Date | any, dayMinSecFlag: boolean) {
     const date = new Date(timestamp);
@@ -28,4 +29,30 @@ export function timestampToTime(timestamp: Date | any, dayMinSecFlag: boolean) {
         return Y + M + D;
     }
     return Y + M + D + h + m + s;
+}
+
+//判断一个字符串中出现次数最多的字符及次数
+export function maxstrString(stringList: string): string {
+    console.log(stringList.split('').sort(), '排序')
+    var maxLength = 0;
+    var maxStr = '';
+    var count = 1;
+    // 比较字符串相邻位置是否相同
+    for (let i = 0; i < stringList.length; i++) {
+        // 如果相同，说明字符串相同，让count +1
+        if (stringList[i] == stringList[i + 1]) {
+            ++count;
+            // maxLength是最大的出现个数，如果相邻个数大于maxLength则说明字符串个数比定义的最大个数大，则最大个数设置为它，最大值为单前的值
+            if (count > maxLength) {
+                console.log(count, '123');
+
+                maxLength = count;
+                maxStr = stringList[i];
+            }
+        } else {
+            count = 1
+        }
+    }
+    return '出现次数最多' + maxStr + '出现次数最多个数' + maxLength
+
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <div>{{ "17639895136" | demo}}</div>
+    <div>{{ "17639895136" | demo }}</div>
     <div>{{ name }}</div>
     <div>{{ data }}</div>
     <ul v-for="(item,index) in list" :key="index">
@@ -21,7 +21,7 @@ import {Component, Vue} from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import {ListParams} from '@/types/index'
 import publicMethod from '@/mixins/PublicMethod'
-import {processingTel} from '@/utils/utils'
+import {processingTel,maxstrString} from '@/utils/utils'
 
 @Component({
   components: {
@@ -29,7 +29,7 @@ import {processingTel} from '@/utils/utils'
   },
   mixins: [publicMethod],
   filters: {
-    demo: (data:string) => {
+    demo: (data: string) => {
       return processingTel(data)
     }
   }
@@ -44,16 +44,69 @@ export default class Home extends Vue {
     name: '小小胖',
     value: 18
   }]
+
   created() {
     this.name = this.mixinTextFun('小小胖')
     processingTel('17639895136')
+    this.sanyuan()
+    maxstrString('adsafsfgadsdaasssssaasssdfssss')
+    console.log(maxstrString('adsafsfgadsdaasssssaasssdfssss'))
   }
 
-//   filters:{
-//     // tel:(data:number){}
-//   //   tel(data:any){
-//   //   //return processingTel(value)
-//   // }
-// }
+  sanyuan(): void {
+    let hungry = true
+    let eat = hungry == true ? 'yes' : 'no'
+    //删除重复项
+    let array = [100, 23, 23, 23, 23, 67, 45]
+    console.log(new Set(array))
+    let outputArray = Array.from(new Set(array))
+    console.log(outputArray)
+    console.log('********for+定时器***********')
+    //定时器它是异步执行的，而for循环执行速度又很快，所以导致当开始执行定时器时。for循环已经执行完毕，所以它一直会输出最后一个i
+    for (let i = 0; i < 8; i++) {
+      setTimeout(function () {
+        console.log(i, 'let')
+      })
+    }
+    for (var i = 0; i < 8; i++) {
+      setTimeout(function () {
+        console.log(i, 'var')
+      })
+    }
+    console.log('**********this的理解*************')
+    // //箭头函数是匿名函数 不能作为构造函数 不能使用this
+    // let funjiantou=()=>{
+    //   console.log('箭头函数')
+    // }
+    // let fc =new funjiantou() 会报错
+    let obj = {
+      a: 10,
+      b: () => { //this 指向window
+        // console.log(this.a) undefined
+      },
+      c: function () { //this 指向当前对象
+        console.log(this.a)//10
+
+      },
+      d: function () {
+        return () => { //箭头函数 this 指向上下文 此时this的指向是当前的对象
+          console.log(this.a,'0000')
+          console.log(this,'d')
+        }
+      }
+    }
+    console.log(obj.d()(),'000')
+    let user = {
+      count: 1,
+      getCount: function () {
+        return this.count
+      }
+    }
+    console.log(user.getCount())
+    let func = user.count
+    console.log(func)
+  }
+
+
 }
 </script>
